585a465791b2c7faf2981aad4d1351559d9d9460
VadyaS <notfoundmyname@gmail.com>
2025-11-20 14:45:16 +0300

    Add files via upload

---
+++ fib.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int fib(int n)
+{
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+  int main() 
+    {
+        int n = 20;
+        for (int i = 0; i < n; i++) {
+          std::cout << fib(i) << " ";
+        }
+        return 0;
+    }
+
+
+
