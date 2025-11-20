eeb4639880d86aa49d668bb3f40d03376de9d038
VadyaS <notfoundmyname@gmail.com>
2025-11-20 15:03:03 +0300

    Add files via upload

---
+++ fib2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+
+int fib(int n)
+{
+    if (n <= 1) return n; 
+    vector<int> num(n + 1);
+    num[0]{ 0 };
+    num[1]{1};
+
+    for (int i{2}; i <= n; i++)
+    {
+        num[i] = num[i - 1] + num[i - 2];
+        return num[n];
+    }
+}
+
+  int main() 
+    {
+      int n{ 20 };
+        for (int i{ 0 }; i < n; ++i)
+        {
+            std::cout << fib(i)<<" ";
+        }
+        std::cout << std::endl;
+        return 0;
+    }
+
+
+
