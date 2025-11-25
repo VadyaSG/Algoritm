a710baf4c596cdfd51990c897eeca0283181f072
VadyaS <notfoundmyname@gmail.com>
2025-11-25 20:02:35 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,40 +1,24 @@
 #include<iostream>
 
-int elem(int* arr, int point,int size);
+
 
 int main()
 {
-	int size{ 9 };
-	int arr[9]{14, 16, 19, 32, 32, 32, 56, 69, 72 };
-	int point{};
+	bool stop{ true };
+	while (stop)
+	{
+		int a{ 0 };
+		int b{ 1 };
+		c = a + b;
+		 b=c;
+		 std::cout << b;
+		 if (b == 30)
+		 {
+			 stop = false;
+		 }
 
-	std::cout << "Enter the point: ";
-	std::cin >> point;
-	std::cout << "Count of elem: " << elem(arr, point, size);
+	}
 	
 	return 0;
 }
 
-int elem(int* arr, int point,int size)
-{
-	int low{ 0 };
-	int high{ size - 1 };
-	int first_index{ size };
-
-	while (low <= high)
-	{
-		int mid = low + (high - low) / 2;
-		if (arr[mid] > point)
-		{
-			first_index = mid;
-			high = mid - 1;
-		
-		}
-		else
-		{
-			low = mid + 1;
-		}
-	}
-	
-	return size - first_index;
-}
